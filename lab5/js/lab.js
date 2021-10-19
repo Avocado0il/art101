b2a45e378025d92a50344d37825251e37bd49fa5
Conner Murphy <murphyconner1@gmail.com>
2021-10-18 19:27:38 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,27 @@
+/*
+* Author:    Conner Murphy
+* Created:  10.18.2021
+*
+*/
+
+// Define Variables
+make = "ford";
+model = "Taurus";
+color = "rusty";
+year = 1995;
+str = new String()
+
+//calculate
+age = 2019 - year;
+
+//output
+str = ("Make: " + make + "<br>");
+document.writeln(str.fontcolor("red"));
+str = ("Model: " + model + "<br>");
+document.writeln(str.fontcolor("red"));
+str = ("Color: " + color + "<br>");
+document.writeln(str.fontcolor("red"));
+str = ("Year: " + year + "<br>");
+document.writeln(str.fontcolor("red"));
+str = ("Age: " + age + " years<br>");
+document.writeln(str.fontcolor("red"));
