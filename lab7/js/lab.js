e5db41159865d8a58cf8de0eaaaf0a3f9befa735
Conner Murphy <murphyconner1@gmail.com>
2021-10-25 15:39:12 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -11,6 +11,9 @@ function sortUserName() {
   // split string to array
   var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
+  // sort The array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
   // join array back to string
   var nameSorted = nameArraySort.join('');
   console.log("nameSorted =", nameSorted);
