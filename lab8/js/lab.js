b1a9b7b8b21d96fa213889965b78c380c9deda25
Conner Murphy <murphyconner1@gmail.com>
2021-10-27 20:36:34 -0700

    Update lab.js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -24,3 +24,6 @@ var result = array.map(function(x){
 })
 // should return [10, 9, 2, 4, 6.4, 12, 100]
 console.log("Squareroot of array:", result);
+
+var outputEl = document.getElementById("output");
+  outputEl.innerHTML = mapResults; // put your results here
